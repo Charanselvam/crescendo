672626166b145c231c93a4df3961b401fb0a04a4
Charanselvam <charanselvam.csk@gmail.com>
2022-12-14 05:31:52 +0530

    blog page alignment

--- src/Screen/PostCardScreen.js
+++ src/Screen/PostCardScreen.js
@@ -1,20 +1,24 @@
-import React from 'react'
-import { Image } from '@chakra-ui/image';
+import React from "react";
+import { Image } from "@chakra-ui/image";
 import { useParams } from "react-router-dom";
 import CardContents from "../Content";
-import {  Box} from '@chakra-ui/layout';
+import { VStack, Text, Heading } from "@chakra-ui/react";
 
 export default function PostCardScreen() {
-    const { id } = useParams();
-    const post = CardContents[id - 1];
-    const { image, name, description } = post;
+  const { id } = useParams();
+  const post = CardContents[id - 1];
+  const { image, name, description } = post;
   return (
-    <div>
-          <Image src={process.env.PUBLIC_URL +  image} alt={name} fluid />
-    
-            <Box>
-              {description}
-            </Box>
-  </div>
-  )
+    <VStack spacing={12}>
+      <Heading>{name}</Heading>
+      <Image
+        src={process.env.PUBLIC_URL + image}
+        alt={name}
+        fluid
+        align="center"
+      />
+
+      <Text>{description}</Text>
+    </VStack>
+  );
 }
